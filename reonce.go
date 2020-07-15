84434f6e66d94b8e680eec531a1f63e03c068cd2
Charlie Vieth <cvieth@lyft.com>
2020-07-15 19:03:01 -0400

    initial commit

---
+++ reonce.go
@@ -0,0 +1,210 @@
+package reonce
+
+import (
+	"io"
+	"regexp"
+	"sync"
+)
+
+type Regexp struct {
+	once sync.Once
+	re   *regexp.Regexp
+	expr string
+}
+
+func New(expr string) *Regexp {
+	return &Regexp{expr: expr}
+}
+
+func (re *Regexp) Compile() (err error) {
+	re.once.Do(func() {
+		re.re, err = regexp.Compile(re.expr)
+	})
+	return err
+}
+
+func (re *Regexp) mustCompile() {
+	re.re = regexp.MustCompile(re.expr)
+}
+
+func (re *Regexp) MustCompile() { re.once.Do(re.mustCompile) }
+
+func (re *Regexp) Copy() *regexp.Regexp {
+	re.once.Do(re.mustCompile)
+	return re.re.Copy()
+}
+
+func (re *Regexp) Expand(dst []byte, template []byte, src []byte, match []int) []byte {
+	re.once.Do(re.mustCompile)
+	return re.re.Expand(dst, template, src, match)
+}
+
+func (re *Regexp) ExpandString(dst []byte, template string, src string, match []int) []byte {
+	re.once.Do(re.mustCompile)
+	return re.re.ExpandString(dst, template, src, match)
+}
+
+func (re *Regexp) Find(b []byte) []byte {
+	re.once.Do(re.mustCompile)
+	return re.re.Find(b)
+}
+
+func (re *Regexp) FindAll(b []byte, n int) [][]byte {
+	re.once.Do(re.mustCompile)
+	return re.re.FindAll(b, n)
+}
+
+func (re *Regexp) FindAllIndex(b []byte, n int) [][]int {
+	re.once.Do(re.mustCompile)
+	return re.re.FindAllIndex(b, n)
+}
+
+func (re *Regexp) FindAllString(s string, n int) []string {
+	re.once.Do(re.mustCompile)
+	return re.re.FindAllString(s, n)
+}
+
+func (re *Regexp) FindAllStringIndex(s string, n int) [][]int {
+	re.once.Do(re.mustCompile)
+	return re.re.FindAllStringIndex(s, n)
+}
+
+func (re *Regexp) FindAllStringSubmatch(s string, n int) [][]string {
+	re.once.Do(re.mustCompile)
+	return re.re.FindAllStringSubmatch(s, n)
+}
+
+func (re *Regexp) FindAllStringSubmatchIndex(s string, n int) [][]int {
+	re.once.Do(re.mustCompile)
+	return re.re.FindAllStringSubmatchIndex(s, n)
+}
+
+func (re *Regexp) FindAllSubmatch(b []byte, n int) [][][]byte {
+	re.once.Do(re.mustCompile)
+	return re.re.FindAllSubmatch(b, n)
+}
+
+func (re *Regexp) FindAllSubmatchIndex(b []byte, n int) [][]int {
+	re.once.Do(re.mustCompile)
+	return re.re.FindAllSubmatchIndex(b, n)
+}
+
+func (re *Regexp) FindIndex(b []byte) (loc []int) {
+	re.once.Do(re.mustCompile)
+	return re.re.FindIndex(b)
+}
+
+func (re *Regexp) FindReaderIndex(r io.RuneReader) (loc []int) {
+	re.once.Do(re.mustCompile)
+	return re.re.FindReaderIndex(r)
+}
+
+func (re *Regexp) FindReaderSubmatchIndex(r io.RuneReader) []int {
+	re.once.Do(re.mustCompile)
+	return re.re.FindReaderSubmatchIndex(r)
+}
+
+func (re *Regexp) FindString(s string) string {
+	re.once.Do(re.mustCompile)
+	return re.re.FindString(s)
+}
+
+func (re *Regexp) FindStringIndex(s string) (loc []int) {
+	re.once.Do(re.mustCompile)
+	return re.re.FindStringIndex(s)
+}
+
+func (re *Regexp) FindStringSubmatch(s string) []string {
+	re.once.Do(re.mustCompile)
+	return re.re.FindStringSubmatch(s)
+}
+
+func (re *Regexp) FindStringSubmatchIndex(s string) []int {
+	re.once.Do(re.mustCompile)
+	return re.re.FindStringSubmatchIndex(s)
+}
+
+func (re *Regexp) FindSubmatch(b []byte) [][]byte {
+	re.once.Do(re.mustCompile)
+	return re.re.FindSubmatch(b)
+}
+
+func (re *Regexp) FindSubmatchIndex(b []byte) []int {
+	re.once.Do(re.mustCompile)
+	return re.re.FindSubmatchIndex(b)
+}
+
+func (re *Regexp) LiteralPrefix() (prefix string, complete bool) {
+	re.once.Do(re.mustCompile)
+	return re.re.LiteralPrefix()
+}
+
+func (re *Regexp) Longest() {
+	re.once.Do(re.mustCompile)
+	re.re.Longest()
+}
+
+func (re *Regexp) Match(b []byte) bool {
+	re.once.Do(re.mustCompile)
+	return re.re.Match(b)
+}
+
+func (re *Regexp) MatchReader(r io.RuneReader) bool {
+	re.once.Do(re.mustCompile)
+	return re.re.MatchReader(r)
+}
+
+func (re *Regexp) MatchString(s string) bool {
+	re.once.Do(re.mustCompile)
+	return re.re.MatchString(s)
+}
+
+func (re *Regexp) NumSubexp() int {
+	re.once.Do(re.mustCompile)
+	return re.re.NumSubexp()
+}
+
+func (re *Regexp) ReplaceAll(src, repl []byte) []byte {
+	re.once.Do(re.mustCompile)
+	return re.re.ReplaceAll(src, repl)
+}
+
+func (re *Regexp) ReplaceAllFunc(src []byte, repl func([]byte) []byte) []byte {
+	re.once.Do(re.mustCompile)
+	return re.re.ReplaceAllFunc(src, repl)
+}
+
+func (re *Regexp) ReplaceAllLiteral(src, repl []byte) []byte {
+	re.once.Do(re.mustCompile)
+	return re.re.ReplaceAllLiteral(src, repl)
+}
+
+func (re *Regexp) ReplaceAllLiteralString(src, repl string) string {
+	re.once.Do(re.mustCompile)
+	return re.re.ReplaceAllLiteralString(src, repl)
+}
+
+func (re *Regexp) ReplaceAllString(src, repl string) string {
+	re.once.Do(re.mustCompile)
+	return re.re.ReplaceAllString(src, repl)
+}
+
+func (re *Regexp) ReplaceAllStringFunc(src string, repl func(string) string) string {
+	re.once.Do(re.mustCompile)
+	return re.re.ReplaceAllStringFunc(src, repl)
+}
+
+func (re *Regexp) Split(s string, n int) []string {
+	re.once.Do(re.mustCompile)
+	return re.re.Split(s, n)
+}
+
+func (re *Regexp) String() string {
+	re.once.Do(re.mustCompile)
+	return re.re.String()
+}
+
+func (re *Regexp) SubexpNames() []string {
+	re.once.Do(re.mustCompile)
+	return re.re.SubexpNames()
+}
---
+++ reonce_test.go
@@ -0,0 +1,204 @@
+package reonce
+
+import (
+	"bytes"
+	"io"
+	"math/rand"
+	"reflect"
+	"regexp"
+	"runtime"
+	"strings"
+	"sync"
+	"testing"
+	"testing/quick"
+	"time"
+)
+
+func TestBadPattern(t *testing.T) {
+	defer func() {
+		if recover() == nil {
+			t.Fatal("expected a panic")
+		}
+	}()
+	New("*").MustCompile()
+}
+
+func TestCompile(t *testing.T) {
+	re := New("a")
+	err := re.Compile()
+	if err != nil {
+		t.Fatal(err)
+	}
+	// make sure we used the sync.Once
+	re.once.Do(func() {
+		t.Fatal("Compile() did not use the sync.Once")
+	})
+}
+
+func TestCompileError(t *testing.T) {
+	const BadPattern = "*"
+	const ErrMessage = "missing argument to repetition operator"
+	err := New(BadPattern).Compile()
+	if err == nil || !strings.Contains(err.Error(), ErrMessage) {
+		t.Errorf("Compile: expected error to contain: %q got: %v", ErrMessage, err)
+	}
+}
+
+func buildMethodArgs(t *testing.T, method reflect.Value) []reflect.Value {
+	rr := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	args := make([]reflect.Value, method.Type().NumIn())
+
+	for i := 0; i < method.Type().NumIn(); i++ {
+		typ := method.Type().In(i)
+		switch typ.Kind() {
+		case reflect.Func:
+			// We need to use real funcs here in case they are called
+			if typ.In(0).Kind() == reflect.String {
+				fn := func(s string) string {
+					return s + s
+				}
+				args[i] = reflect.ValueOf(fn)
+			} else {
+				fn := func(s []byte) []byte {
+					return append(s, s...)
+				}
+				args[i] = reflect.ValueOf(fn)
+			}
+		case reflect.Interface:
+			// We need to use a real io.RuneReader here in case it's called
+			args[i] = reflect.ValueOf((io.RuneReader)(&bytes.Buffer{}))
+		case reflect.String:
+			args[i] = reflect.ValueOf("aaa")
+		case reflect.Slice:
+			switch typ.Elem().Kind() {
+			case reflect.Uint8:
+				args[i] = reflect.ValueOf([]byte("aaa"))
+			case reflect.Int:
+				args[i] = reflect.ValueOf([]int{1, 2})
+			default:
+				t.Errorf("Invalid slice type: %s", typ.String())
+			}
+		default:
+			var ok bool
+			args[i], ok = quick.Value(typ, rr)
+			if !ok {
+				t.Fatalf("Failed to create value for Type: %s", typ)
+			}
+		}
+	}
+
+	return args
+}
+
+func TestLazyCompile(t *testing.T) {
+	const GoodPattern = "a"
+
+	testMethod := func(t *testing.T, methodName string) {
+		re := New(GoodPattern)
+
+		m := reflect.ValueOf(re).MethodByName(methodName)
+
+		m.Call(buildMethodArgs(t, m))
+		if re.re == nil {
+			t.Error("Failed to initialize re.re")
+		}
+		if re.String() != GoodPattern {
+			t.Errorf("Want expr: %q got: %q", GoodPattern, re.re.String())
+		}
+	}
+
+	typ := reflect.TypeOf(&Regexp{})
+	for i := 0; i < typ.NumMethod(); i++ {
+		m := typ.Method(i)
+		if m.Name == "Compile" {
+			continue
+		}
+		t.Run(m.Name, func(t *testing.T) {
+			testMethod(t, m.Name)
+		})
+	}
+}
+
+func TestLazyCompilePanic(t *testing.T) {
+	const BadPattern = "*"
+	const ErrMessage = "missing argument to repetition operator"
+
+	testPanic := func(t *testing.T, methodName string) {
+		e := recover()
+		switch v := e.(type) {
+		case nil:
+			t.Errorf("%s: should have panicked", methodName)
+		case string:
+			if !strings.Contains(v, ErrMessage) {
+				t.Errorf("%s: expected error message to contain: %q got: %q",
+					methodName, ErrMessage, v)
+			}
+		default:
+			t.Errorf("%s: unexpect panic type: %T", methodName, e)
+		}
+	}
+
+	testMethod := func(t *testing.T, methodName string) {
+		re := New(BadPattern)
+
+		m := reflect.ValueOf(re).MethodByName(methodName)
+
+		defer testPanic(t, methodName)
+		m.Call(buildMethodArgs(t, m))
+	}
+
+	typ := reflect.TypeOf(&Regexp{})
+	for i := 0; i < typ.NumMethod(); i++ {
+		m := typ.Method(i)
+		if m.Name == "Compile" {
+			continue
+		}
+		t.Run(m.Name, func(t *testing.T) {
+			testMethod(t, m.Name)
+		})
+	}
+}
+
+func TestLazyCompileParallel(t *testing.T) {
+	const expr = `(|(((((((((x{1}){1,7}){1,2}){1,2}){2}){2}){2}){2}){2})?` +
+		`(((((((((x{1}){2,3}){2}){2}){2}){2}){2}){2}){2}){2})*`
+	re := New(expr)
+	start := make(chan struct{})
+	wg := new(sync.WaitGroup)
+	ready := new(sync.WaitGroup)
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		ready.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			ready.Done()
+			<-start
+			for i := 0; i < 100; i++ {
+				if !re.MatchString("xxxxxxx") {
+					t.Errorf("%d: failed to match string", i)
+					return
+				}
+			}
+		}(i)
+	}
+
+	ready.Wait()
+	close(start)
+	wg.Wait()
+}
+
+func BenchmarkInitOverhead(b *testing.B) {
+	re := New("a")
+	for i := 0; i < b.N; i++ {
+		re.Longest() // cheapest method
+	}
+}
+
+func BenchmarkInitOverhead_Baseline(b *testing.B) {
+	re := regexp.MustCompile("a")
+	for i := 0; i < b.N; i++ {
+		re.Longest() // cheapest method
+	}
+}
