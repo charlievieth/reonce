77d5fd7962e6aa2db635cb72d114edaa47e7a32c
Charlie Vieth <charlie.vieth@gmail.com>
2021-12-14 14:06:32 -0500

    recache: improve test coverage (#5)

--- recache/recache_test.go
+++ recache/recache_test.go
@@ -35,8 +35,8 @@ func quote(s string) string {
 	return strconv.Quote(s)
 }
 
-func TestCacheSize(t *testing.T) {
-	c := New(8)
+func testCacheSize(t *testing.T, fn func(int) *Cache) {
+	c := fn(8)
 	for i := 0; i < 16; i++ {
 		c.MustCompile(strconv.Itoa(i))
 	}
@@ -66,12 +66,31 @@ func TestCacheSize(t *testing.T) {
 	}
 }
 
-func TestNegativeCacheSize(t *testing.T) {
-	mustPanic(t, "New(-1)", func() { New(-1) })
+func TestCacheSize(t *testing.T) {
+	testCacheSize(t, New)
 }
 
-func TestUnlimitedCacheSize(t *testing.T) {
-	c := New(0)
+func TestCacheSizePOSIX(t *testing.T) {
+	testCacheSize(t, NewPOSIX)
+}
+
+func mustPanic(t *testing.T, msg string, fn func()) {
+	t.Helper()
+	defer func() {
+		if recover() == nil {
+			t.Fatal("test did not panic:", msg)
+		}
+	}()
+	fn()
+}
+
+func TestNegativeCacheSize(t *testing.T) {
+	mustPanic(t, "New(-1)", func() { New(-1) })
+	mustPanic(t, "NewPOSIX(-1)", func() { NewPOSIX(-1) })
+}
+
+func testUnlimitedCacheSize(t *testing.T, fn func(int) *Cache) {
+	c := fn(0)
 	for i := 0; i < 1024; i++ {
 		c.MustCompile(strconv.Itoa(i))
 	}
@@ -84,24 +103,44 @@ func TestUnlimitedCacheSize(t *testing.T) {
 	}
 }
 
+func TestUnlimitedCacheSize(t *testing.T) {
+	testUnlimitedCacheSize(t, New)
+}
+
+func TestUnlimitedCacheSizePOSIX(t *testing.T) {
+	testUnlimitedCacheSize(t, NewPOSIX)
+}
+
 func TestCompile(t *testing.T) {
+	// TODO: this uses the global caches - consider changing
+	if _, err := Compile(`a`); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := CompilePOSIX(`a`); err != nil {
+		t.Fatal(err)
+	}
+
 	// make sure an error is always returned
 	for i := 0; i < 3; i++ {
 		if _, err := Compile(`[a`); err == nil {
 			t.Errorf("Compile(`[a`): expected error got: %v", err)
 		}
 	}
+	for i := 0; i < 3; i++ {
+		if _, err := CompilePOSIX(`[a`); err == nil {
+			t.Errorf("Compile(`[a`): expected error got: %v", err)
+		}
+	}
 }
 
 // make sure we're thread-safe
-func TestCompileParallel(t *testing.T) {
+func testCompileParallel(t *testing.T, format string, fn func(int) *Cache) {
 	if testing.Short() {
 		t.Skip("short test")
 	}
-	const format = `(?P<foo>.*)(?P<bar>(a)b(%d)?)(?P<foo>.*)a`
 
 	var wg sync.WaitGroup
-	c := New(124)
+	c := fn(124)
 	n := new(int64)
 	start := make(chan struct{})
 	for i := 0; i < 4; i++ {
@@ -109,7 +148,7 @@ func TestCompileParallel(t *testing.T) {
 		go func() {
 			defer wg.Done()
 			<-start
-			for i := 0; i < 4096; i++ {
+			for i := 1; i <= 4096; i++ {
 				expr := fmt.Sprintf(format, atomic.AddInt64(n, 1))
 				c.MustCompile(expr)
 			}
@@ -136,14 +175,15 @@ func TestCompileParallel(t *testing.T) {
 	}
 }
 
-func mustPanic(t *testing.T, msg string, fn func()) {
-	t.Helper()
-	defer func() {
-		if e := recover(); e == nil {
-			t.Fatal("test did not panic:", msg)
-		}
-	}()
-	fn()
+// make sure we're thread-safe
+func TestCompileParallel(t *testing.T) {
+	const format = `(?P<foo>.*)(?P<bar>(a)b(%d)?)(?P<foo>.*)a`
+	testCompileParallel(t, format, New)
+}
+
+func TestCompileParallelPOSIX(t *testing.T) {
+	const format = `.{1}{2}.{3}.%d`
+	testCompileParallel(t, format, NewPOSIX)
 }
 
 func TestMustCompile(t *testing.T) {
@@ -155,23 +195,37 @@ func TestMustCompile(t *testing.T) {
 	}
 }
 
-// func TestMustCompilePOSIX(t *testing.T) {
-// 	mustPanic(t, "MustCompilePOSIX: `[a`", func() { MustCompilePOSIX(`[a`) })
-//
-// 	re := MustCompilePOSIX("^abc$")
-// 	if !re.MatchString("abc") {
-// 		t.Fatal("failed to match string")
-// 	}
-// }
+func TestMustCompilePOSIX(t *testing.T) {
+	mustPanic(t, "MustCompilePOSIX: `[a`", func() { MustCompilePOSIX(`[a`) })
 
-func TestSetMaxEntries(t *testing.T) {
-	c := New(15)
+	re := MustCompilePOSIX("^abc$")
+	if !re.MatchString("abc") {
+		t.Fatal("failed to match string")
+	}
+}
+
+func testSetMaxEntries(t *testing.T, fn func(int) *Cache) {
+	c := fn(15)
 	for i := 'a'; i < 'a'+20; i++ {
 		c.MustCompile(string(i))
 	}
 	if c.Len() != 15 {
 		t.Fatalf("Len: got: %d want: %d", 15, c.Len())
 	}
+	// Make sure the expected entries are there
+	for i := 'a' + 5; i < 'a'+20; i++ {
+		key := string(i)
+		ee, ok := c.cache[key]
+		if !ok {
+			t.Errorf("Evicted key: %q", key)
+		}
+		exp := ee.re.String()
+		got := c.MustCompile(key).String()
+		if exp != got {
+			t.Errorf("MustCompile(`%s`) = %q want: %q", key, got, exp)
+		}
+	}
+
 	c.SetMaxEntries(10)
 	if c.Len() != 10 {
 		t.Fatalf("Len: got: %d want: %d", 10, c.Len())
@@ -189,6 +243,51 @@ func TestSetMaxEntries(t *testing.T) {
 	mustPanic(t, "SetMaxEntries(-1)", func() {
 		c.SetMaxEntries(-1)
 	})
+}
+
+func TestSetMaxEntries(t *testing.T) {
+	testSetMaxEntries(t, New)
+
+	// Test global cache
+	t.Run("Global", func(t *testing.T) {
+		testSetMaxEntries(t, func(n int) *Cache {
+			for Len() != 0 {
+				std.removeOldest()
+			}
+			SetMaxEntries(n)
+			if MaxEntries() != n {
+				t.Fatalf("MaxEntries: got: %d want: %d", MaxEntries(), n)
+			}
+			return std
+		})
+	})
+}
+
+func TestSetMaxEntriesPOSIX(t *testing.T) {
+	testSetMaxEntries(t, NewPOSIX)
+
+	// Test global cache
+	t.Run("Global", func(t *testing.T) {
+		testSetMaxEntries(t, func(n int) *Cache {
+			for LenPOSIX() != 0 {
+				posix.removeOldest()
+			}
+			SetMaxEntriesPOSIX(n)
+			if MaxEntriesPOSIX() != n {
+				t.Fatalf("MaxEntriesPOSIX: got: %d want: %d", MaxEntriesPOSIX(), n)
+			}
+			return posix
+		})
+	})
+}
+
+func TestGlobals(t *testing.T) {
+	if std.POSIX() {
+		t.Error("std.POSIX() should be false")
+	}
+	if !posix.POSIX() {
+		t.Error("posix.POSIX() should be true")
+	}
 }
 
 const benchRe = `[\pL\pN][^\pL\pN]|(.$)`
